8e641b5a9f754d08ed075c617a2e4524b9e5128e
Atyansh <atyansh.jaiswal@gmail.com>
2014-02-06 17:40:26 -0800

    Solutions

---
+++ Euler23.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class Euler23
+{
+  LinkedList<Integer> abundant = new LinkedList<Integer>();
+
+  public static void main(String arg[])
+  {
+    long sum = 0;
+    
+    int sum1 = 1;
+
+    int sqrt = 0;
+    double sqrt1 = 0;
+
+    Euler23 obj = new Euler23();
+
+    for(int i = 1; i < 28123; i++)
+    {
+      sqrt1 = Math.sqrt(i);
+      sqrt = (int) sqrt1;
+
+      for(int j = 2; j <= sqrt; j++)
+        if(i % j == 0)
+        {  
+          if(j == sqrt && sqrt == sqrt1)
+            sum1 += j;
+          else 
+            sum1 += j + i/j;
+        }
+          
+      if(sum1 > i)
+        obj.abundant.add(i);
+
+      if(!obj.abundantSum(i))
+        sum += i;
+
+      sum1 = 1;
+
+    }
+
+    System.out.println(sum);
+  }
+
+
+  boolean abundantSum(int n)
+  {
+    Object[] a = abundant.toArray();
+    Integer[] arr = Arrays.copyOf(a, a.length, Integer[].class);
+
+    int size = arr.length;
+
+    for(int i = 0; i < size; i++)
+    {
+      for(int j = size-1; j >= i; j--)
+      {
+        if((arr[i] + arr[j]) < n)
+          break;
+        else if((arr[i] + arr[j]) == n)
+          return true;
+      }
+    }
+
+    return false;
+  }
+}
---
+++ Euler24.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+class Euler24
+{
+  public static void main(String arg[])
+  {
+    String a = "0123456789";
+
+    char[] arr = a.toCharArray();
+
+    for(int i = 1; i < 1000000; i++)
+      Euler24.next(arr, 9);
+    
+    System.out.println(new String(arr));
+  }
+
+
+  public static void next(char[] arr, int index)
+  {
+    if(arr[index] < arr[index - 1])
+      next(arr, index-1);
+    else
+    {
+      int min = 10;
+      int i2 = 15;
+      
+      for(int i = index; i < arr.length; i++)
+        if(arr[i] - arr[index-1] < min && arr[i] - arr[index-1] > 0)
+        {
+            min = arr[i] - arr[index-1];
+            i2 = i;
+        }
+
+        arr[index-1] += min;
+        arr[i2] -= min;
+
+      Arrays.sort(arr, index, arr.length);
+    }
+          
+  }
+}
+
