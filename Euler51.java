5587eac44e00b2944c893ad244aac7be82af897b
Atyansh Jaiswal <atjaiswa@ucsd.edu>
2014-06-08 22:00:58 -0700

    Solutions

---
+++ Euler51.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+class Euler51
+{
+  public static void main(String arg[])
+  {
+    long num = 3;
+
+    int count = 0;
+
+    while(count != 8)
+    {
+      num += 2;
+
+      if(!isPrime(num))
+        continue;
+
+      count = calculate("" + num);
+      //System.err.println(num + " " + count);
+    }
+    
+    System.out.println(num);
+  }
+  
+
+  public static int calculate(String num)
+  {
+    int length = num.length();
+    int pivots = (int) Math.pow(2, length);
+
+    int count = 0;
+    int temp;
+
+    for(int i = 1; i < pivots; i++)
+    {
+      temp = 0;
+      
+      String bin = Integer.toBinaryString(i);
+      //System.err.println(bin);
+      ArrayList<Integer> arr = new ArrayList<Integer>(bin.length());
+      for(int j = bin.length()-1, k = 0; j >= 0; j--, k++)
+      {
+        if(bin.charAt(j) == '1')
+          arr.add(k);
+      }
+      for(char ch = '0'; ch <= '9'; ch++)
+      {
+        char[] digits = num.toCharArray();
+        for(Integer index : arr)
+          digits[length-index.intValue()-1] = ch;
+
+        if(isPrime(Long.parseLong(new String(digits))) && digits[0] != '0')
+        {
+          //System.out.println(new String(digits));
+          temp++;
+        }
+      }
+      if(temp == 8)
+        System.err.printf("Pivots: %0" + length + "d%n", Integer.parseInt(Integer.toBinaryString(i)));
+
+      if(temp > count)
+        count = temp;
+    }
+    return count;
+  }
+
+  public static boolean isPrime(long n)
+  {
+    if(n < 2)
+      return false;
+
+    if(n == 2)
+      return true;
+
+    if(n % 2 == 0)
+      return false;
+
+    long sqrt = (long) Math.sqrt(n);
+
+    for(long i = 3; i <= sqrt; i+=2)
+      if(n % i == 0)
+        return false;
+
+    return true;
+  }
+}
---
+++ Euler59.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+
+class Euler59
+{
+  public static void main(String[] arg)
+  {
+    File file = new File("cipher1.txt");
+    
+    Scanner scan;
+
+    try
+    {
+      scan = new Scanner(file);
+    }
+    catch(FileNotFoundException e)
+    {
+      System.err.println("File not found.");
+      return;
+    }
+
+    String line = scan.nextLine();
+
+    String[] code = line.split(",");
+
+    int[] tokens = new int[code.length];
+
+    for(int i = 0; i < tokens.length; i++)
+      tokens[i] = Integer.parseInt(code[i]);
+
+    int total = 0;
+    int temp;
+    int answer = 0;
+    int tempAnswer;
+    int tempValue;
+
+    for(int i = 97; i < 123; i++)
+    {
+      for(int j = 97; j < 123; j++)
+      {
+        for(int k = 97; k < 123; k++)
+        {
+          temp = 0;
+          tempAnswer = 0;
+
+          int x = 0;
+          for(; x < tokens.length - 2; x += 3)
+          {
+            tempValue = tokens[x] ^ i;
+            tempAnswer += tempValue;
+            if(tempValue == 32)
+              temp++;
+
+            tempValue = tokens[x+1] ^ j;
+            tempAnswer += tempValue;
+            if(tempValue == 32)
+              temp++;
+
+            tempValue = tokens[x+2] ^ k;
+            tempAnswer += tempValue;
+            if(tempValue == 32)
+              temp++;
+          }
+
+          if(x < tokens.length)
+            tempAnswer += tokens[x] ^ i;
+          if( (x+1) < tokens.length)
+            tempAnswer += tokens[x+1] ^ j;
+
+          if(temp > total)
+          {
+            total = temp;
+            answer = tempAnswer;
+          }
+        }
+      }
+    }
+
+    System.out.println(answer);
+  }
+}
